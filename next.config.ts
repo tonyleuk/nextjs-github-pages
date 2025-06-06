3bc8d093fcd1f8c101db25cea00f1ee38fc14d0c
tonyleuk <tonyleuk@gmail.com>
2025-06-06 10:17:54 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
   output: "export",
 
   /**
-   * Set base path. This is usually the slug of your repository.
+   * Set base path. This is the slug of your GitHub repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
@@ -19,7 +19,7 @@ const nextConfig: NextConfig = {
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
    *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
    */
   images: {
     unoptimized: true,
